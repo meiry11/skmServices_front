5aa8c5a56bee7696759f07f1c5ac050746fc35f0
meiry11 <meirycisse11@gmail.com>
2022-10-19 06:25:51 +0200

    creation de la branche service

--- src/app/services/Adresse/adresse-service.service.ts
+++ src/app/services/Adresse/adresse-service.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Adresse } from 'src/app/models/Adresse.model';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 const API_URL_Adresse = environment.API_URL+"/adresse"
 
@@ -8,5 +10,14 @@ const API_URL_Adresse = environment.API_URL+"/adresse"
 })
 export class AdresseService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  
+  public getAdresses(){
+    return this.http.get<Adresse[]>(API_URL_Adresse)
+    
+  }
+
+  public save(newAdresse:Adresse){
+    return this.http.post<Adresse>(API_URL_Adresse , newAdresse)
+  }
 }
